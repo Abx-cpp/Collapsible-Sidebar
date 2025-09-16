db462dcdcb2b0a2f3805c879420d092721bd4e22
abubakar.afzal <l233062@lhr.nu.edu.pk>
2025-09-16 10:34:38 +0500

    Adding project files

---
+++ index.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+toggleBtn.addEventListener('click', () => {
+    if(window.innerWidth > 768){
+        sidebar.classList.toggle('collapsed');
+    }
+    else{
+        sidebar.classList.toggle('show');
+    }
+});
